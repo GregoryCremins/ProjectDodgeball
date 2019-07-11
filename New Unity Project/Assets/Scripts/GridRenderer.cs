7d20d98b2cd97cc77cbc3b29ad9be2c7c2ea45f2
Gregory Cremins <coolwhar@gmail.com>
2019-07-10 23:44:21 -0400

    InitCommit

---
+++ New Unity Project/Assets/Scripts/GridRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridRenderer : MonoBehaviour {
+
+    public GameObject myGridSpace;
+    private GridLayoutGroup myGrid;
+
+    // Use this for initialization
+    void Start () {
+        /* render the grid */
+        myGrid = gameObject.GetComponent<GridLayoutGroup>();
+        for(int i = 0; i < 9; i++)
+        {
+            GameObject newGridSpace = Instantiate(myGridSpace);
+            newGridSpace.transform.SetParent(transform);
+            
+        }
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
