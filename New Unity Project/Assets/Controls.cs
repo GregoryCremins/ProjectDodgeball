b4a558ebb364a74dc28ef0d29e7d8826fa1c6835
Gregory Cremins <coolwhar@gmail.com>
2019-11-17 11:51:00 -0500

    Starting menu

---
+++ New Unity Project/Assets/Controls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+    public GameObject startTarget;
+    public GameObject controllerObject;
+    public GameObject mouseAndKeyBoardObject;
+    // Use this for initialization
+    void Start() {
+        if (PlayerPrefs.GetInt("ControllerEnabled") == 1)
+        {
+            //set controller object
+        }
+		else
+        if(PlayerPrefs.GetInt("MouseAndKeyboardEnabled") == 1)
+        {
+            //set mouseAndKeyboard Controls
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
