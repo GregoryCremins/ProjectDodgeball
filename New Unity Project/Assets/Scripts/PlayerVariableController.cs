4abf476f4af8af09b035b9766ad944c2a02043ed
Gregory Cremins <coolwhar@gmail.com>
2019-12-20 22:12:19 -0500

    Initative stuff

---
+++ New Unity Project/Assets/Scripts/PlayerVariableController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVariableController : MonoBehaviour
+{
+
+    public float Player1Energy = 100;
+    public float Player2Energy = 100;
+    public float Player3Energy = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addToEnergy(int playerNumber, int addValue)
+    {
+        if (playerNumber == 1)
+        {
+            Player1Energy = Player1Energy + addValue;
+        }
+        if (playerNumber == 2)
+        {
+            Player2Energy = Player2Energy + addValue;
+        }
+        if (playerNumber == 3)
+        {
+            Player3Energy = Player3Energy + addValue;
+        }
+    }
+}
